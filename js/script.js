856fea5c37d82df77ef143dccaa60298e419ba20
connor farrell <farr0232@algonquinlive.com>
2022-08-18 18:52:06 -0400

    adds future structure to html, adds beginning js, adds hiding elements, adds datepicker, adds max day to current day so no future dates can be selected

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// initilize the datepicker and format the y-m-d and set max date to today so no future dates can be selected
+$("#datepicker").datepicker({ maxDate: '0', dateFormat: "yy-mm-dd" });
+
+// hide elements on page
+$("#current_search").hide();
+$("#liked_content_div").hide();
